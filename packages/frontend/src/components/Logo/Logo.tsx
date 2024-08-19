c401fb68d9d1912c985d90bb4a945ceb7a12f2fd
Dmitry Aronov <aronov.mml@gmail.com>
2024-08-20 03:38:19 +0500

    fix import filename

--- packages/frontend/src/components/Logo/Logo.tsx
+++ packages/frontend/src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from './logo.module.css';
+import styles from './Logo.module.css';
 
 export const Logo = () => {
   return (
